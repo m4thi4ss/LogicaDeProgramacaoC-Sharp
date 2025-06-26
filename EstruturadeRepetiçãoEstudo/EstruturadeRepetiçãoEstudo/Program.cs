264ea31033468b98e4efadba9d4183de843cdc67
João Mathias <joao.mathias@gmail.com>
2025-06-26 17:49:25 -0300

    Finalizando a explicação de cada exercicio feito

--- EstruturadeRepetiçãoEstudo/EstruturadeRepetiçãoEstudo/Program.cs
+++ EstruturadeRepetiçãoEstudo/EstruturadeRepetiçãoEstudo/Program.cs
@@ -124,7 +124,7 @@ Console.WriteLine(media1);*/
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 /*### **6. Somatório de números positivos**
 
-Escreva um programa que peça números ao usuário e some-os, parando quando o usuário inserir um número negativo.*/
+Escreva um programa que peça números ao usuário e some-os, parando quando o usuário inserir um número negativo.
 
 int num; //int - Seria um tipo de dado de uma variavel; num - seria o nome da variavel. Ele esta fora do DO por que ele precisa ser existir pra que consiga fazer a estrutura de repetição.
 int soma = 0; //int - Seria o tipo de dado da variavel; soma - Seria o nome da variavel, nesse caso tem que colocar 0, por que vai ser acrescentado valores dentro dessa variavel, para que no final do laço ela informe quanto foi adicionado.
@@ -160,4 +160,143 @@ do
 
 } while (num1 >= 0);
 
-Console.WriteLine(soma1);
+Console.WriteLine(soma1);*/
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*### **7. Número primo**
+
+Peça ao usuário um número e determine se ele é primo.
+
+using System.Runtime.InteropServices;
+int resultado; //int - Seria o tipo de dado da variavel; resultado - Seria o nome da variavel; Basicamente ela foi criada para receber o valor dentro do for.
+
+
+Console.WriteLine("Me informe um número: "); //Console.WriteLine - Seria uma mensagem que vai ser enviada para o usuario no console, nesse caso ele vai solicitar um numero.
+int num = int.Parse(Console.ReadLine()); //int - Seria um tipo de dado; num - Seria o nome da variavel; int.Parse - Aqui ele vai informar que não vai ser uma string e sim um int; Console.ReadLine - Aqui ele vai ler a informação da variavel num e vai trasnformar em int. Sendo assim ele criou uma variavel chamada num com o tipo de dado int para guardar a informação que vai ser passada pelo usuario, como o tipo de dado vai ser um int eu usei o int.Parse para avisar e o Console.ReadLine ele vai ler a informação e vai de trasnformar de string pra int, pois toda WriteLine transforma a informação passada pelo usuario em string.
+
+for(int i = 2; i < num; i++) // for - O FOR ele serve para verificar a informação que foi passado, se a informação for verdadeira ele continua rodando, so para quando a informação for falsa; () - Seria o parametro do for; i = 2 - Seria o inicio da estrutura de repetição; i < num - Seria o fim da estrutura de repetição. i++ - Seria o incremento onde ele vai adicionar 1 até chegar no falso. Nesse caso ele vai analisar as informações que vão começar pelo numero 2 e vai até o num, quando passar do num, o laço de repetição para, usando o i++ para incrementar 1 numero até chegar a zero.
+{
+    resultado = num % i; //resultado - Ele seria a variavel que vai ser usada para fazer a lista dos resultados do resto da divisão da lista criada pelo for. num - Seria a variavel que vai guardar a informação do usuario; i - Vai ser a informação o laço de repetição criado pelo for. Sendo assim aqui foi criado para fazer a divisão do num / i e essa divisão vai dar o resto, transformando o resultado em uma lista de resto de divisão.
+
+    if(resultado == 0 ) // IF - Seria um e SE, nesse caso se a lista do resultado ter um 0 ele vai retornar o bloco de codigo.
+    {
+        Console.WriteLine("Não é primo"); //Console.WriteLine - Seria uma mensagem que vai ser enviada para o usuario no console, nesse caso se if for verdadeiro ele vai retornar uma mensagem para cada 0 na lista que não é um primo.
+        break; //Break - Ele força a parada do laço de repetição, basicamente ele viu que tem 0 na lista do resultado, ele não vai mandar um por um, ele vai mandar de uma vez só, que não é primo.
+    }
+    else //Else - Seria o SE NÃO, se o if der falso ele vai usar o bloco de codigo do else, seguindo as informações do parametro acima.
+    {
+        Console.WriteLine("É primo"); //Console.WriteLine - Seria uma mensagem que vai ser enviada para o usuario no console, nesse caso ele vai informar que não é primo.
+        break; //Break - Ele serve para forçar a para do laço de repetição, nesse caso se aparecer algum 1 ele vai retornar de uma vez e em uma unica vez o bloco de codigo, sem mandar um por um, criando uma nova lista.
+    }
+}
+
+int resultado1;
+
+Console.WriteLine("Me informe um numero: ");
+int num1 = int.Parse(Console.ReadLine());
+
+for (int i1 = 2; i1 < num1; i1++)
+{
+    resultado1 = num1 % i1;
+
+    if (resultado1 == 0)
+    {
+        Console.WriteLine("Não é numero primo");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Numero é primo");
+        break;
+    }
+}*/
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*### **8. Sequência de Fibonacci**
+
+Peça um número N ao usuário e exiba os N primeiros termos da sequência de Fibonacci.
+
+using System.Reflection;
+
+Console.WriteLine("Me informe até onde a lista de Fibonaci vai: "); //Console.WriteLine - Aqui ele vai mandar uma mensagem para o usuari, nesse caso ele vai solicitar até onde a lista da fibanacci vai ir.
+int num = int.Parse(Console.ReadLine()); //int - Aqui seria o tipo de dado da variavel; num - Seria o nome da variavel; int.Parse - Aqui ele vai informar que o tipo de dado vai ser um int e não uma string; Console.ReadLine - Ele serve para ler a informação passada pelo usuario e transformar de string para int. Sendo assim criou um WriteLine para mandar uma mensagem para o usuario solicitando até onde vai a lista Fibonacci, e criou uma variavel para guardar a informação que vai ser passada pelo usuario, a variavel vai ter um tipo de dado int, como vai ser um int avisei com o int.Parse e coloquei o ReadLine para ler a informação passada e transformar de string para int.
+int primeiro = 0; //int - Seria o tipo de dado da variavel; primeiro - Seria o nome da variavel. Nesse caso essa variavel esta sendo usada para mostrar na tela a lista fibonacci.
+int segundo = 1; //int - Seria o tipo de dado da variavel; segundo - Seria o nome da variavel. Essa variavel vai ser usada para auxiliar no resultado da primeira, por isso vai começar com o 1, para ajudar no calculo.
+
+for (int i = 0; i < num ; i++) //for - Seria um laço de repetição; int - Seria o tipo de dado da variavel "i"; i = 0 - Seria o inicio do loop; i < num - Seria o final do loop; i++ - Seria o icremento para que o loop chegue até o final do loop. Sendo assim criei um for para informar que vai ter um paramentro com regra que vai ter um tipo de dado int, onde vai começar com 0 e vai terminar na informação que a variavel "num" guardou para o usuario, tendo o incremento para rodar até chegar na variavel.
+{
+    // Calcula o próximo número da sequência
+    int proximo = primeiro + segundo; // int - Seria o tipo de dado da variavel; proximo - Seria o nome da variavel; primeiro - Seria o nome da variavel; segundo - Seria o nome da variavel; Sendo assim foi criado uma variavel onde ele vai guardar o resultado da soma das variaveis primeiro(0) e segundo(1)
+
+    // Atualiza os valores para o próximo passo
+    primeiro = segundo; //Aqui ele vai pegar a informação da variavel segundo que veio antes de ele pegar o resultado do calculo, por ex ele vai pegar a informação que esta va variavel segundo, que foi usada no calculo e não a do resultado, por isso essa varivel esta recebendo o resultado da variavel segundo, primeiro que quando a variavel segundo recebe a informação do proximo.
+    segundo = proximo; Sendo assim ele vai usar o console.Write para mostrar na mesma linha o resultado, então ele vai mostrar a primeira variavel, fazendo o primeiro lopping:
+        mostrar = 0 
+        
+        proximo = primeiro(0) + segundo (1)
+        proximo = 1 (O proximo vai ficar 1 por conta do primeiro + segundo e vai dar o restultado)
+        segundo = 1 (O segundo vai continuar sendo 1 por conta que ele vai pegar a informação que a variavel proximo guardou, por que ele vai ficar com essa informação por causa que ele informou que segundo = proximo, sendo assim toda rodada que for feito a soma do primeiro + segundo o proximo vai guardar o resultado e passar para o segundo na proxima rodada)
+        primeiro = 1 (Por que ele vira 1, por que o segundo ele ja vem com o 1, então automaticamente o primeiro vai para o 1, e por isso que no proximo ele vai ser 1 também por que o segundo vai continuar sendo 1, por que vai 0 + 1 = 1 dando o resultado da variavel proximo.
+
+       segundo lopping
+        mostrar = 1 (Por que na segunda rodada vai mostrar "1" por que o primeiro ficou com o 1 do segundo  )
+
+        proximo = primeiro (1) + segundo (1)
+        proximo = 2 (O proximo vai herdar o resultado do calculo do primeiro + segundo)
+        segundo = 2 (E o segundo vai herdar a informação que foi guardada com o proximo)
+        primeiro = 1 (E o primeiro vai guardar a iformação que veio com o segundo)
+
+    
+
+    Console.Write("N" + primeiro + " " ); // Mostra o número atual
+    
+    
+};
+Console.WriteLine(" ");
+
+Console.WriteLine("Me informe até onde vai a lista Fibonnaci: ");
+int num1 = int.Parse(Console.ReadLine());
+
+int primeiro1 = 0;
+int segundo1 = 1;
+
+for (int i = 0; i < num1; i++)
+{
+    int proximo1 = primeiro1 + segundo1;
+
+    primeiro1 = segundo1;
+    segundo1 = proximo1;
+
+    Console.Write("N" + primeiro1 + " ");
+}*/
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*### **9. Adivinhe o número**
+
+O programa escolhe um número aleatório entre 1 e 100. O usuário tenta adivinhar e o programa dá dicas.*/
+
+int escolhido; //int - Esse seria o tipo de dado; escolhido - Seria o nome da variavel; Ele vai usar essa variavel dentro do DO para colocar um numero aleatorio.
+int num = 0; //int - Esse seria o tipo de dado; num - Seria o nome da variavel; Ele vai ser usado para guardar a informação passada pelo usuario;
+do //O do ele seria um laço de repetição, onde ele vai rodar o bloco de codigo até o while verificar que é verdadeiro a informação passada pelo usuario pra ele para o codigo.
+{
+    Random random = new Random(); //Random - Seria uma classe criada pelo .Net; random - Seria a variavel que vai guardar essa classe Radom; new Random() - Aqui ele vai instaciar o objeto com o construtor para fazer com que de a vida a essa classe que foi guardada no random. Sendo assim aqui ele vai criar uma variavel random para usar o metodo Next que fica dentro da classe Random, e o new random literalmente vai dar vida a variavel random, onde ele vai instanciar a classe para objeto com o contrutor onde ele vai da a vida para aquela variavel, para usar o metodo .Next que tem dentro da classe Random.
+    
+ 
+    escolhido = random.Next(0, 101); //escolhido - Seria o nome da variavel; random - vai ser a variavel que guarda a classe random; .Next - Seria o metodo da classe Random onde ele vai gerar um numero aleatorio seguindo a regra do paramentro; (0, 101) - Seria a regra do parametro do .Next.
+
+        Console.WriteLine("Adivinhe o numero que o programa escolheu:"); //Console.WriteLine - Seria uma mensagem enviada para o usuario no console, nesse caso ele vai solicitar para o usuario para adivinhar qual numero o program escolheu.
+        num = int.Parse(Console.ReadLine()); //num - Seria o nome da variavel; int.Parse - Aqui ele está informando que o tipo de dado vai ser um int e não uma string; Console.ReadLine - Ele vai ler a informação passada pelo usuario e vai transformar de string para int; Sendo assim 
+
+        if (num == escolhido)
+        {
+            Console.WriteLine("Você acertou!");
+        }
+        else if (num < escolhido)
+        {
+            Console.WriteLine("Esse numero é menor!!!");
+        }
+        else
+        {
+            Console.WriteLine("Esse numero é maior!!!");
+        }
+    
+} while(num != escolhido);
+
